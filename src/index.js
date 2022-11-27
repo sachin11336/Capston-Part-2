db5b5bc377a064e90f9493ce7161fc895fb615f9
sachin11336 <107074376+sachin11336@users.noreply.github.com>
2022-11-27 14:24:44 +0530

    Project Statement for the Product Details Page

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Login from "./components/Auth/Login";
 import SignUp from "./components/Auth/SignUp";
-=======
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
->>>>>>> cec88d1d74bee6f32a12b9e025af1c99ee887285
+import Products from "./components/products/Products";
+
+import Details from "./components/Details/Details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <Router>
-      <App />
-      <Routes>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signUp" element={<SignUp></SignUp>}></Route>
+ <Router>
+    <App />
+
+
+    <Routes>
+         <Route path="/signUp" element={<SignUp></SignUp>}></Route>
         <Route path="/" element={<Login></Login>}></Route>
       </Routes>
     </Router>
-=======
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
->>>>>>> cec88d1d74bee6f32a12b9e025af1c99ee887285
+
   </React.StrictMode>
 );
 
